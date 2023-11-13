239e3a9e43ceb6394094f63074f554db91a5753c
Matt Saemrow <mattsaemrow@gmail.com>
2023-11-13 15:36:40 -0600

    cleaned up code and add loop for local storage. still not working though

--- app.js
+++ app.js
@@ -6,17 +6,15 @@ const toDoList = document.querySelector('#todo-list')
 //Local Storage Array
 const toDoListArray = JSON.parse(localStorage.getItem('toDOListArray')) || [];
 
-//Event listener that adds completed class to item if completed button is clicked
-//or removes item if Remove button is clicked
-toDoList.addEventListener('click', function(e){
-    if(e.target.id==='completedItem'){
-        e.target.parentElement.classList.add('completed');
-        e.target.parentElement.classList.remove('incomplete');
-        console.log(e.target.parentElement.firstChild.data);
-    } else if(e.target.id==='removeItem'){
-        e.target.parentElement.remove();
+for(let i=0; i<toDoListArray.length; i++){
+    let newToDo = document.createElement('li');
+    newToDo.innerText = toDoListArray[i].task;
+    newToDo.isComplete = toDoListArray[i].isComplete ? true : false;
+    if(newToDo.isComplete){
+        newToDo.classList.add('completed');
     }
-})
+    toDoList.append(newToDo);
+}
 
 
 //Event listener that adds a new li, completed button and remove button
@@ -44,3 +42,23 @@ form.addEventListener('submit', function(e){
     input.value = '';
     toDoList.appendChild(newItem);
 })
+
+//Event listener that adds completed class to item if completed button is clicked
+//or removes item if Remove button is clicked
+toDoList.addEventListener('click', function(e){
+    if(e.target.id==='removeItem'){
+        e.target.parentElement.remove();
+    }
+    else if(!e.target.isComplete){
+        e.target.parentElement.classList.add('completed');
+        e.target.parentElement.classList.remove('incomplete');
+        e.target.isComplete = true;
+        console.log("Completd: " + e.target.isComplete);
+    }
+    else if(e.target.isComplete){
+        e.target.parentElement.classList.add('incomplete');
+        e.target.parentElement.classList.remove('completed');
+        e.target.isComplete = false;
+    }
+}
+);
